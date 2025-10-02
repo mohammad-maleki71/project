24258bfc94c6d97f6685938ea0cd35143db22e4b
mmd <malekimohammad950@gmail.com>
2025-10-02 17:10:46 +0330

    add useradmin and superuser

--- M/account/admin.py
+++ M/account/admin.py
@@ -1,2 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import Group
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('phone_number', 'email', 'full_name', 'is_admin')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+    (None, {'fields':('phone_number', 'email', 'full_name', 'password')}),
+    ('Permissions', {'fields':('is_admin','is_active', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields':('phone_number', 'email', 'full_name', 'password1', 'password2')}),
+    )
+
+    search_fields = ('email','full_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+
+
+
+
+
 
